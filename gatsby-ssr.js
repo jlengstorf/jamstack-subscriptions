9a3180930b24d808b821d44159fa19ab4b721b85
Jason Lengstorf <jason@lengstorf.com>
2020-05-13 08:35:49 -0700

    wip: back to basics

--- gatsby-ssr.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { IdentityContextProvider } from 'react-netlify-identity-widget';
-
-export const wrapRootElement = (element) => {
-  return <IdentityContextProvider>{element}</IdentityContextProvider>;
-};
