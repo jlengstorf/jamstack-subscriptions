c3a31482fa5bd4ef6f0f523b4bccf1640db5a6de
Jason Lengstorf <jason@lengstorf.com>
2020-05-13 09:07:00 -0700

    wip: store Netlify user ID in Fauna

--- functions/identity-signup.js
+++ functions/identity-signup.js
@@ -1,6 +1,39 @@
+const fetch = require('node-fetch');
+
 exports.handler = async (event) => {
   const { user } = JSON.parse(event.body);
   console.log(JSON.stringify(user, null, 2));
+
+  const netlifyID = user.id;
+
+  // TODO create a Stripe customer object
+  const stripeID = 1;
+
+  // TODO create a customer record in Fauna
+  const response = await fetch('https://graphql.fauna.com/graphql', {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${process.env.FAUNA_SERVER_KEY}`,
+    },
+    body: JSON.stringify({
+      query: `
+        mutation ($netlifyID: ID! $stripeID: ID!) {
+          createUser(data: {netlifyID: $netlifyID, stripeID: $stripeID}) {
+            netlifyID
+            stripeID
+          }
+        }
+      `,
+      variables: {
+        netlifyID,
+        stripeID,
+      },
+    }),
+  })
+    .then((res) => res.json())
+    .catch((err) => console.error(err));
+
+  console.log({ response });
 
   return {
     statusCode: 200,
