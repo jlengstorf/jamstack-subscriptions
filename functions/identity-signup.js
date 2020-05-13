d171fae576af6d37425ee6569552b04b6ac6bab5
Jason Lengstorf <jason@lengstorf.com>
2020-05-13 08:56:23 -0700

    wip: plzwork

--- functions/identity-signup.js
+++ functions/identity-signup.js
@@ -2,7 +2,8 @@ exports.handler = async (event) => {
   const { user } = JSON.parse(event.body);
   console.log(JSON.stringify(user, null, 2));
 
-  return JSON.stringify({
-    app_metadata: { roles: ['sub:free'] },
-  });
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ app_metadata: { roles: ['sub:free'] } }),
+  };
 };
