220a1a3f1f10cca4ae7bd5c1a3888282ae0ec424
Jason Lengstorf <jason@lengstorf.com>
2020-05-13 08:51:36 -0700

    wip: log out user info, add role

--- functions/identity-signup.js
+++ functions/identity-signup.js
@@ -1,8 +1,8 @@
-exports.handler = async (...args) => {
-  console.log(JSON.stringify(args, null, 2));
+exports.handler = async (event) => {
+  const { user } = JSON.parse(event.body);
+  console.log(JSON.stringify(user, null, 2));
 
-  return {
-    statusCode: 200,
-    body: 'ok',
-  };
+  return JSON.stringify({
+    app_metadata: { roles: ['sub:free'] },
+  });
 };
